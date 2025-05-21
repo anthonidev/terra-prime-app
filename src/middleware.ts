a4cdc523a01b0f5bc4d416bc8cc21412e4b1022e
CodingMarin <marinalexander691@gmail.com>
2025-05-20 19:19:49 -0500

    improved: login design changed
    - add format images and excluded paths in middleware
    - add the show state password
    - remove the background animation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import { withAuth } from "next-auth/middleware";
-import { getToken } from "next-auth/jwt";
+import { NextResponse } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
 export default withAuth(
   async function middleware(req) {
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
     const isAuth = !!token;
-    const isAuthPage = req.nextUrl.pathname === "/auth/login";
+    const isAuthPage = req.nextUrl.pathname === '/auth/login';
     if (isAuthPage && isAuth) {
-      return NextResponse.redirect(new URL("/", req.url));
+      return NextResponse.redirect(new URL('/', req.url));
     }
     if (!isAuthPage && !isAuth) {
-      return NextResponse.redirect(new URL("/auth/login", req.url));
+      return NextResponse.redirect(new URL('/auth/login', req.url));
     }
     return NextResponse.next();
   },
@@ -18,12 +18,22 @@ export default withAuth(
     callbacks: {
       async authorized() {
         return true;
-      },
-    },
+      }
+    }
   }
 );
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|public).*)",
-  ],
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images (folder with images)
+     * - public (folder with public assets)
+     * - image extensions (png, jpg, etc.)
+     */
+    '/((?!api|_next/static|_next/image|favicon\\.ico|images|public|.*\\.(?:png|jpg|jpeg|gif|svg|ico|webp)$).*)'
+  ]
 };
